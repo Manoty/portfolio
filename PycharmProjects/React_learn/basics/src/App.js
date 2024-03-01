2257803d453c22f8c0cc777af429a43f37528364
kevz <kevinnoty21@gmail.com>
2024-03-01 10:40:34 +0300

    changed home section appearance

---
+++ PycharmProjects/React_learn/basics/src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import TodoList from "./TodoList";
+function App() {
+  return (
+      <>
+        <TodoList/>
+        <input type='text' />
+        <button>Add Todo</button>
+        <button>Clear Completed</button>
+        <div>0 left to do</div>
+      </>
+
+  )
+}
+
+export default App;
